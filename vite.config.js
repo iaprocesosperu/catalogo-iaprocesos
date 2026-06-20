4c87309bd6e35c5c3c59729266920b17b0405d86
IA Procesos <nlope81@gmail.com>
2026-06-20 01:50:58 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
-export default defineConfig({
-  plugins: [react(), basicSsl()],
-})
+export default defineConfig({ plugins: [react()] })
